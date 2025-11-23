c6dd588f56c7ff710c4d43b53909edca79907184
Hissal <hissalofficial@gmail.com>
2025-11-23 18:09:16 +0200

    Add HMath class with ClampToFloat and Map methods for value manipulation

---
+++ src/HCommons/Math/HMathExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HCommons.Math;
+
+public static class HMathExtensions {
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float ClampToFloat(this double value, float min = float.MinValue, float max = float.MaxValue) {
+        return HMath.ClampToFloat(value, min, max);
+    }
+    
+    public static int Map(this int value, int fromSource, int toSource, int fromTarget, int toTarget) {
+        return HMath.Map(value, fromSource, toSource, fromTarget, toTarget);
+    }
+    
+    public static float Map(this float value, float fromSource, float toSource, float fromTarget, float toTarget) {
+        return HMath.Map(value, fromSource, toSource, fromTarget, toTarget);
+    }
+    
+    public static double Map(this double value, double fromSource, double toSource, double fromTarget, double toTarget) {
+        return HMath.Map(value, fromSource, toSource, fromTarget, toTarget);
+    }
+}
