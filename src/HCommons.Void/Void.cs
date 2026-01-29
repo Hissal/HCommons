39941c95f65d44cbe5c1c83f3c57eae8764796c6
Lassi Harjaluoma <89788928+Hissal@users.noreply.github.com>
2026-01-29 12:53:26 +0200

    feat: introduce Void type for representing absence of value (#55)

---
+++ src/HCommons.Void/Void.cs
@@ -0,0 +1,12 @@
+﻿namespace HCommons.Void;
+
+/// <summary>
+/// Represents a type that has no value or state. This is useful in scenarios where a type is required
+/// but no meaningful value needs to be conveyed, such as in generic constraints or as a placeholder.
+/// </summary>
+public readonly record struct Void {
+    /// <summary>
+    /// Gets the singleton instance of the <see cref="Void"/> type.
+    /// </summary>
+    public static Void Value => new Void();
+}
